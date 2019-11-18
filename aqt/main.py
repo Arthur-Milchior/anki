f1d11a6ac2fa7031bff3ccfad7f9edca1ad71c1d
Arthur Milchior <Arthur@Milchior.fr>
2019-11-18 04:25:57 +0100

    onEmptyCards: cids is a set

--- aqt/main.py
+++ aqt/main.py
@@ -1275,7 +1275,7 @@ will be lost. Continue?"""))
         """Method called by Tools>Empty Cards..."""
 
         self.progress.start(immediate=True)
-        cids = self.col.emptyCids()
+        cids = set(self.col.emptyCids())
         if not cids:
             self.progress.finish()
             tooltip(_("No empty cards."))
