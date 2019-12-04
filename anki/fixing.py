c4db1362f0e578efaf7191ddd0131c82f334cf65
Arthur Milchior <Arthur@Milchior.fr>
2019-12-04 22:24:00 +0100

    Fixing.checkDeck

--- anki/fixing.py
+++ anki/fixing.py
@@ -54,6 +54,7 @@ class FixingManager:
         self.floatIvlInCard()
         self.floatIvlInRevLog()
         self.ensureSomeNoteType()
+        self.checkDeck()
 
     def noteWithMissingModel(self):
         # note types with a missing model
@@ -204,3 +205,17 @@ and type = {CARD_NEW}""", [intTime(), self.col.usn()])
         # models
         if self.col.models.ensureNotEmpty():
             self.problems.append("Added missing note type.")
+
+    def checkDeck(self):
+        """check that all default confs/decks option are set in all deck's related object"""
+        for paramsSet, defaultParam, what, kind in [(self.col.decks.dconf.values(), defaultDeckConf, "'s option", "deck configuration"),
+                                                    (self.col.decks.all(sort=False, standard=True, dyn=False), defaultDeck, "", "standard deck"),
+                                                    (self.col.decks.all(sort=False, standard=False, dyn=True), defaultDynamicDeck, " (dynamic)", "dynamic deck"),
+                                                    (self.col.decks.all(sort=False, standard=False, dyn=True), defaultDeckConf, " (dynamic)", "dynamic deck as conf"),
+        ]:
+            for key in defaultParam:
+                for params in paramsSet:
+                    if key not in params:
+                        params[key] = defaultParam[key]
+                        self.col.decks.save(params)
+                        self.problems.append(f"Adding some «{key}» which was missing in deck{what} {params['name']}")
