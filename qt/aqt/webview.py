4d5a8417dabd95db7d9c64806023e394afd192b3
Arthur Milchior <Arthur@Milchior.fr>
2020-05-11 17:58:41 +0200

    b->msg

--- qt/aqt/webview.py
+++ qt/aqt/webview.py
@@ -87,8 +87,8 @@ class AnkiWebPage(QWebEnginePage):
             level = "warning"
         elif level == QWebEnginePage.ErrorMessageLevel:
             level = "error"
-        buf = "JS %(t)s %(f)s:%(a)d %(b)s" % dict(
-            t=level, a=line, f=srcID, b=msg + "\n"
+        buf = "JS %(t)s %(f)s:%(a)d %(msg)s" % dict(
+            t=level, a=line, f=srcID, msg=msg + "\n"
         )
         # ensure we don't try to write characters the terminal can't handle
         buf = buf.encode(sys.stdout.encoding, "backslashreplace").decode(
