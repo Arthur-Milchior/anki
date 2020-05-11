54d7534fd98a3d72632b25ce818d1af485008df0
Arthur Milchior <Arthur@Milchior.fr>
2020-05-11 19:11:35 +0200

    s->filt

--- qt/aqt/browser.py
+++ qt/aqt/browser.py
@@ -1133,7 +1133,7 @@ QTableView {{ gridline-color: {grid} }}
             item = SidebarItem(
                 name,
                 ":/icons/heart.svg",
-                lambda s=filt: self.setFilter(s),  # type: ignore
+                lambda filt=filt: self.setFilter(filt),  # type: ignore
             )
             root.addChild(item)
 
